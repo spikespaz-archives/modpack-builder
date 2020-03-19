201a33cc5d212fd12a95bc223e110aaadd1d49c9
Jacob Birkett <spikespaz@outlook.com>
2020-03-19 11:08:26 -0700

    Add function to add a launcher profile

--- downloader.py
+++ downloader.py
@@ -3,11 +3,19 @@ import requests
 import sys
 import os
 import arrow
+import secrets
+
+from pathlib import Path
 
 API_BASE_URL = "https://api.cfwidget.com/minecraft/mc-mods"
 
 
 class VersionException(Exception):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class ProfileExistsException(Exception):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -60,6 +68,54 @@ def get_mod_info(mod_slug, game_versions):
     mod_data["releases"] = releases
 
     return mod_data
+
+
+def get_profile_id(id_file):
+    """
+    Create a 32 character hexadecimal token, and write it to the file. Fetch if the file exists.
+    """
+    if id_file.exists():
+        with open(id_file, "r") as file:
+            profile_id = file.read()
+    else:
+        profile_id = secrets.token_hex(16)
+
+        with open(id_file, "w") as file:
+            file.write(profile_id)
+
+
+def add_launcher_profile(minecraft, profile_dir, profile_name, profile_icon, java_args, java_dir, version_id):
+    minecraft = Path(minecraft)
+    profiles = None
+    profiles_file = minecraft / "launcher_profiles.json"
+    profile_id_file = profile_dir / "profile_id"
+    profile_id = get_profile_id(profile_id_file)
+
+    with open(profiles_file, "r") as file:
+        profiles = json.load(file)
+
+    if profile_id in profiles["profiles"]:
+        raise ProfileExistsException("Profile already in launcher profiles: " + profile_id)
+    else:
+        utc_now = arrow.utcnow().replace("+00:00", "Z")
+
+        profiles["profiles"][profile_id] = {
+            "created": utc_now,
+            "gameDir": str(profile_dir.resolve()),
+            "icon": profile_icon,
+            "javaArgs": java_args,
+            "javaDir": str(java_dir.resolve()),
+            "lastUsed": utc_now,
+            "lastVersionId": version_id,
+            "name": profile_name,
+            "type": "custom"
+        }
+
+        profiles["selectedUser"]["profile"] = profile_id
+
+        with open(profiles_file, "w") as file:
+            json.dump(profiles, file, indent=2)
+
 
 if __name__ == "__main__":
     pack_meta = None
