8cf0e6d7afdadae9cd18b3c4ee3a39c86cac77c8
Jacob Birkett <spikespaz@outlook.com>
2020-07-21 12:30:33 -0700

    Add static methods to determine good RAM amounts

--- modpack_builder/builder2.py
+++ modpack_builder/builder2.py
@@ -40,15 +40,15 @@ class ModpackBuilder:
         self.forge_minecraft_version = ""
         self.forge_version = ""
 
-        self.java_runtime_memory = 0
+        self.java_runtime_memory = self._get_recommended_memory()
         self.java_runtime_arguments = []
 
         self.java_download_urls = {}
 
         self.external_resource_globs = []
 
-        self.minecraft_directory = 0
-        self.minecraft_launcher_path = 0
+        self.minecraft_directory = None
+        self.minecraft_launcher_path = None
 
         self.concurrent_requests = 8
         self.concurrent_downloads = 8
@@ -82,3 +82,22 @@ class ModpackBuilder:
 
     def install_external_resources(self):
         pass
+
+    @staticmethod
+    def _get_system_memory():
+        return math.ceil(psutil.virtual_memory().total / 1024 / 1024 / 1024)
+
+    @staticmethod
+    def _get_max_recommended_memory():
+        return ModpackBuilder._get_system_memory() - 1
+
+    @staticmethod
+    def _get_recommended_memory():
+        system_memory = ModpackBuilder._get_system_memory()
+
+        if system_memory == 4:
+            return 3
+        elif system_memory < 8:
+            return system_memory - 2
+        else:
+            return min(system_memory / 2, ModpackBuilder._max_recommended_java_runtime_memory)
