6b71fc6daf5e5dffa4a2f113ba691c6bf479f8f2
Jacob Birkett <spikespaz@outlook.com>
2020-07-23 14:28:41 -0700

    Remove if __name__ is __main__ from module __main__.py

--- modpack_builder/__main__.py
+++ modpack_builder/__main__.py
@@ -1,15 +1,14 @@
+import sys
+
+from qtpy.QtWidgets import QApplication
+
+from .application import ModpackBuilderWindow
 from .builder2 import ModpackBuilder
 
-if __name__ == "__main__":
-    import sys
 
-    from qtpy.QtWidgets import QApplication
+app = QApplication([])
+builder = ModpackBuilder()
+window = ModpackBuilderWindow(builder)
 
-    from .application import ModpackBuilderWindow
-
-    app = QApplication([])
-    builder = ModpackBuilder()
-    window = ModpackBuilderWindow(builder)
-
-    window.show()
-    sys.exit(app.exec_())
+window.show()
+sys.exit(app.exec_())
