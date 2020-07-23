e5fa46f65de718abaa1a88792f104d5a324e2c85
Jacob Birkett <spikespaz@outlook.com>
2020-07-23 14:27:11 -0700

    Remove legacy files, new branch legacy

--- install_modpack_slower.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-title Minecraft Modpack Installer by Jacob Birkett
-
-call "modpack_builder.dist\modpack_builder.exe" -m install -z "modpack.zip" --concurrent-requests 1 --concurrent-downloads 1
-
-set /p=Press ENTER to exit.
--- uninstall_modpack.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-title Minecraft Modpack Installer by Jacob Birkett
-
-call "modpack_builder.dist\modpack_builder.exe" -m uninstall -z "modpack.zip"
-
-set /p=Press ENTER to exit.
