a4adb2fcffa9154336735da051d6f8dfad2497fe
Jacob Birkett <spikespaz@outlook.com>
2020-07-26 01:00:26 -0700

    Add property dictionary to reassemble manifest

--- modpack_builder/builder.py
+++ modpack_builder/builder.py
@@ -128,6 +128,82 @@ class ModpackManifest:
                 server=True
             ))
 
+    @property
+    def dictionary(self):
+        dictionary = dict()
+
+        dictionary["profile_name"] = self.profile_name
+        dictionary["profile_id"] = self.profile_id
+        dictionary["profile_icon"] = self.profile_icon
+        dictionary["game_versions"] = list(self.game_versions)
+        dictionary["java_downloads"] = self.java_downloads._asdict()
+        dictionary["forge_download"] = self.forge_download
+        dictionary["version_label"] = self.version_label
+        dictionary["release_preference"] = self.release_preference.value
+        dictionary["load_priority"] = list(self.load_priority)
+
+        client_data = dict()
+        server_data = dict()
+
+        client_data["java_args"] = " ".join(self.client_java_args)
+        server_data["java_args"] = " ".join(self.server_java_args)
+
+        client_external_files = {
+            "immutable": [],
+            "overwrite": []
+        }
+        server_external_files = {
+            "immutable": [],
+            "overwrite": []
+        }
+
+        for entry in self.external_files:
+            if entry.server and entry.immutable:
+                server_external_files["immutable"].append(entry.pattern)
+            elif entry.server:  # entry.server and not entry.immutable
+                server_external_files["overwrite"].append(entry.pattern)
+            elif entry.immutable:  # not entry.server and entry.immutable
+                client_external_files["immutable"].append(entry.pattern)
+            else:  # not entry.server and not entry.immutable
+                client_external_files["overwrite"].append(entry.pattern)
+
+        client_data["external_files"] = client_external_files
+        server_data["external_files"] = server_external_files
+
+        client_external_mods = dict()
+        server_external_mods = dict()
+
+        for entry in self.external_mods:
+            entry_dict = entry._asdict()
+
+            del entry_dict["identifier"]
+            del entry_dict["server"]
+
+            if entry.server:
+                server_external_mods[entry.identifier] = entry_dict
+            else:
+                client_external_mods[entry.identifier] = entry_dict
+
+        client_data["external_mods"] = client_external_mods
+        server_data["external_mods"] = server_external_mods
+
+        client_curseforge_mods = list()
+        server_curseforge_mods = list()
+
+        for entry in self.curseforge_mods:
+            if entry.server:
+                server_curseforge_mods.append(entry.identifier)
+            else:
+                client_curseforge_mods.append(entry.identifier)
+
+        client_data["curseforge_mods"] = client_curseforge_mods
+        server_data["curseforge_mods"] = server_curseforge_mods
+
+        dictionary["client"] = client_data
+        dictionary["server"] = server_data
+
+        return dictionary
+
 
 class ModpackBuilder:
     # I would love to find a way to make everything below into static,
