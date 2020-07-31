b49f8da2527096845641a56a203782d89b294b32
Jacob Birkett <spikespaz@outlook.com>
2020-07-31 00:09:11 -0700

    Move text validators to own file

--- modpack_builder/application.py
+++ modpack_builder/application.py
@@ -10,9 +10,9 @@ from subprocess import Popen
 import markdown2
 
 from qtpy import uic
+from qtpy.QtGui import QDesktopServices, QPixmap
 from qtpy.QtWebEngineWidgets import QWebEnginePage
 from qtpy.QtWidgets import QMainWindow, QHeaderView
-from qtpy.QtGui import QDesktopServices, QPixmap, QValidator
 from qtpy.QtCore import Qt, QModelIndex, QSysInfo, QEvent, QItemSelection
 
 from . import helpers
@@ -21,68 +21,8 @@ from . import utilities
 from .builder import ModpackBuilder
 from .curseforge import ReleaseType
 from .models import LoadingPriorityTableModel
+from .validators import SlugValidator, PathValidator
 from .multi_progress_dialog import MultiProgressDialog
-
-
-class SlugValidator(QValidator):
-    def __init__(self, parent=None, **kwargs):
-        super().__init__(parent)
-
-        self.__kwargs = kwargs
-
-        # Allow the user to input whitespace at the end by default
-        if "rstrip" not in self.__kwargs:
-            self.__kwargs["rstrip"] = False
-
-    def validate(self, text, cursor_pos):
-        return (
-            QValidator.Acceptable,
-            utilities.slugify(text, **self.__kwargs),
-            min(cursor_pos, len(text))
-        )
-
-
-class PathValidator(QValidator):
-    def __init__(self, parent=None, file=False, extensions=None):
-        super().__init__(parent)
-
-        self.__file = file
-        self.__extensions = extensions
-
-    def validate(self, text, cursor_pos):
-        path = Path(text)
-        validity = None
-
-        if not path.exists():
-            if path.parent.exists() and path.parent.is_dir():
-                # If the parent directory exists the path has been valid until the last part was added.
-                # The state should be intermediate because it could be a typo or in-between changes.
-                validity = QValidator.Intermediate
-            else:
-                # The parent either doesn't exist or is a file.
-                # Return an invalid state to prevent the user from typing and not realizing the mistake.
-                validity = QValidator.Invalid
-        elif self.__file and path.is_file():
-            if self.__extensions and (path.suffix in self.__extensions or "".join(path.suffixes) in self.__extensions):
-                validity = QValidator.Acceptable
-            elif self.__extensions:
-                # The user probably hasn't typed the extension yet, so an intermediate state is returned.
-                # This branch won't be run if `self.__extensions` contains an empty string,
-                # so that the user can match any filename without an extension.
-                validity = QValidator.Intermediate
-            else:
-                # No extensions provided to match, any file is acceptable.
-                validity = QValidator.Acceptable
-        elif self.__file and path.is_dir():
-            # The current text is probably a parent of the final file path.
-            validity = QValidator.Intermediate
-        elif path.is_dir():
-            # The validator is not expected to match a file and a directory is acceptable.
-            validity = QValidator.Acceptable
-
-        # If the below assertion fails I have made a mistake and did not predict some form of input.
-        assert validity is not None
-        return validity, text, cursor_pos
 
 
 class LockedWebEnginePage(QWebEnginePage):
